dac9545b982601189690b111244bfbccb0c87b1f
Saint Michel ISSA GARBA <saintmichelissagarba@D2SIMBP078.local>
2019-05-29 14:13:45 +0200

    v.2.4 update vpc configuration

--- vpc/main.tf
+++ vpc/main.tf
@@ -130,6 +130,13 @@ resource "aws_security_group" "eks_ssh_sg" {
   name        = "${var.security_group_name}-ssh"
   description = "sg-${var.security_group_description}"
   vpc_id      = "${aws_vpc.eks.id}"
+
+  tags = "${merge(
+    local.common_tags,
+    map(
+      "Name", "eks_ssh_sg"
+    )
+  )}"
 }
 resource "aws_security_group_rule" "ingress_rule_ssh" {
   security_group_id = "${aws_security_group.eks_ssh_sg.id}"
@@ -152,6 +159,13 @@ resource "aws_security_group" "eks_cluster_sg" {
   name        = "cluster-${var.security_group_name}"
   description = "sg-${var.security_group_description}"
   vpc_id      = "${aws_vpc.eks.id}"
+
+  tags = "${merge(
+    local.common_tags,
+    map(
+      "Name", "eks_clusters_sg"
+    )
+  )}"
 }
 resource "aws_security_group_rule" "ingress_rule_cluster" {
   count             = "${length(var.inbound_rules_cluster)}"
@@ -166,6 +180,13 @@ resource "aws_security_group" "eks_nodes_sg" {
   name        = "nodes-${var.security_group_name}"
   description = "sg-${var.security_group_description}"
   vpc_id      = "${aws_vpc.eks.id}"
+
+  tags = "${merge(
+    local.common_tags,
+    map(
+      "Name", "eks_nodes_sg"
+    )
+  )}"
 }
 resource "aws_security_group_rule" "ingress_rule_nodes" {
   count             = "${length(var.inbound_rules_nodes)}"
